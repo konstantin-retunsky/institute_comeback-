cc1038ff3db48b748aba5fd529b498270ad8b3d8
konstantin retunsky <konstantin.retunsky@gmail.com>
2021-12-12 16:59:22 +0500

    init

---
+++ ulearn/4_Risovatel/DrawingProgram.cs
@@ -0,0 +1,95 @@
+using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+
+namespace RefactorMe
+{
+  class Drawer
+  {
+    static float x, y;
+    static Graphics grafika;
+
+    public static void Initialization(Graphics novayaGrafika)
+    {
+      grafika = novayaGrafika;
+      grafika.SmoothingMode = SmoothingMode.None;
+      grafika.Clear(Color.Black);
+    }
+
+    public static void set_position(float x0, float y0)
+    {
+      x = x0;
+      y = y0;
+    }
+
+    public static void makeIt(Pen ruchka, double dlina, double ugol)
+    {
+      //Делает шаг длиной dlina в направлении ugol и рисует пройденную траекторию
+      var x1 = (float) (x + dlina * Math.Cos(ugol));
+      var y1 = (float) (y + dlina * Math.Sin(ugol));
+      grafika.DrawLine(ruchka, x, y, x1, y1);
+      x = x1;
+      y = y1;
+    }
+
+    public static void Change(double dlina, double ugol)
+    {
+      x = (float) (x + dlina * Math.Cos(ugol));
+      y = (float) (y + dlina * Math.Sin(ugol));
+    }
+  }
+
+  public class ImpossibleSquare
+  {
+    public static void Draw(int shirina, int visota, double ugolPovorota, Graphics grafika)
+    {
+      // ugolPovorota пока не используется, но будет использоваться в будущем
+      Drawer.Initialization(grafika);
+
+      var sz = Math.Min(shirina, visota);
+
+      var diagonal_length = Math.Sqrt(2) * (sz * 0.375f + sz * 0.04f) / 2;
+      var x0 = (float) (diagonal_length * Math.Cos(Math.PI / 4 + Math.PI)) + shirina / 2f;
+      var y0 = (float) (diagonal_length * Math.Sin(Math.PI / 4 + Math.PI)) + visota / 2f;
+
+      Drawer.set_position(x0, y0);
+
+      //Рисуем 1-ую сторону
+      Drawer.makeIt(Pens.Yellow, sz * 0.375f, 0);
+      Drawer.makeIt(Pens.Yellow, sz * 0.04f * Math.Sqrt(2), Math.PI / 4);
+      Drawer.makeIt(Pens.Yellow, sz * 0.375f, Math.PI);
+      Drawer.makeIt(Pens.Yellow, sz * 0.375f - sz * 0.04f, Math.PI / 2);
+
+      Drawer.Change(sz * 0.04f, -Math.PI);
+      Drawer.Change(sz * 0.04f * Math.Sqrt(2), 3 * Math.PI / 4);
+
+      //Рисуем 2-ую сторону
+      Drawer.makeIt(Pens.Yellow, sz * 0.375f, -Math.PI / 2);
+      Drawer.makeIt(Pens.Yellow, sz * 0.04f * Math.Sqrt(2), -Math.PI / 2 + Math.PI / 4);
+      Drawer.makeIt(Pens.Yellow, sz * 0.375f, -Math.PI / 2 + Math.PI);
+      Drawer.makeIt(Pens.Yellow, sz * 0.375f - sz * 0.04f, -Math.PI / 2 + Math.PI / 2);
+
+      Drawer.Change(sz * 0.04f, -Math.PI / 2 - Math.PI);
+      Drawer.Change(sz * 0.04f * Math.Sqrt(2), -Math.PI / 2 + 3 * Math.PI / 4);
+
+      //Рисуем 3-ю сторону
+      Drawer.makeIt(Pens.Yellow, sz * 0.375f, Math.PI);
+      Drawer.makeIt(Pens.Yellow, sz * 0.04f * Math.Sqrt(2), Math.PI + Math.PI / 4);
+      Drawer.makeIt(Pens.Yellow, sz * 0.375f, Math.PI + Math.PI);
+      Drawer.makeIt(Pens.Yellow, sz * 0.375f - sz * 0.04f, Math.PI + Math.PI / 2);
+
+      Drawer.Change(sz * 0.04f, Math.PI - Math.PI);
+      Drawer.Change(sz * 0.04f * Math.Sqrt(2), Math.PI + 3 * Math.PI / 4);
+
+      //Рисуем 4-ую сторону
+      Drawer.makeIt(Pens.Yellow, sz * 0.375f, Math.PI / 2);
+      Drawer.makeIt(Pens.Yellow, sz * 0.04f * Math.Sqrt(2), Math.PI / 2 + Math.PI / 4);
+      Drawer.makeIt(Pens.Yellow, sz * 0.375f, Math.PI / 2 + Math.PI);
+      Drawer.makeIt(Pens.Yellow, sz * 0.375f - sz * 0.04f, Math.PI / 2 + Math.PI / 2);
+
+      Drawer.Change(sz * 0.04f, Math.PI / 2 - Math.PI);
+      Drawer.Change(sz * 0.04f * Math.Sqrt(2), Math.PI / 2 + 3 * Math.PI / 4);
+    }
+  }
+}
---
+++ ulearn/branching/Pluralize/PluralizeTask.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Pluralize
+{
+  public static class PluralizeTask
+  {
+    public static string PluralizeRubles(int count)
+    {
+      string countString = count.ToString();
+      string lastTwoSymbol = count >= 10
+        ? countString.Substring(countString.Length - 2, 2)
+        : $"0{count}";
+
+      if (count == 10)
+      {
+        Console.WriteLine(10);
+      }
+
+      if (
+        lastTwoSymbol[1] == '0' ||
+        lastTwoSymbol[1] == '5' ||
+        lastTwoSymbol[1] == '6' ||
+        lastTwoSymbol[1] == '7' ||
+        lastTwoSymbol[1] == '8' ||
+        lastTwoSymbol[1] == '9' ||
+        lastTwoSymbol == "11" ||
+        lastTwoSymbol == "12" ||
+        lastTwoSymbol == "13" ||
+        lastTwoSymbol == "14"
+      )
+        return "рублей";
+      else if (
+        lastTwoSymbol[1] == '1'
+      )
+        return "рубль";
+      else
+        return "рубля";
+    }
+  }
+}
