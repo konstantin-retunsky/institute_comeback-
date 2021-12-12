2bf7a358bdefe31f8c468cc817ae39ebf7d4e746
konstantin retunsky <konstantin.retunsky@gmail.com>
2021-12-12 22:35:22 +0500

    done mazes, mazes test with bug last items

--- ulearn/Cycles/Mazes/EmptyMazeTask.cs
+++ ulearn/Cycles/Mazes/EmptyMazeTask.cs
@@ -1,9 +1,18 @@
-﻿namespace Mazes
+﻿using System;
+
+namespace Mazes
 {
-	public static class EmptyMazeTask
-	{
-		public static void MoveOut(Robot robot, int width, int height)
-		{
-		}
-	}
+  public static class EmptyMazeTask
+  {
+    public static void MoveOut(Robot robot, int width, int height)
+    {
+      while (!robot.Finished)
+      {
+        if (robot.X != width - 2)
+          robot.MoveTo(Direction.Right);
+        else if (robot.Y != height - 2)
+          robot.MoveTo(Direction.Down);
+      }
+    }
+  }
 }
